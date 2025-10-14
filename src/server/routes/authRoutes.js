c056b60e337bf0a927484181cbc8ead491a41efc
Yoonlimin <u6511566@au.edu>
2025-10-15 05:01:42 +0700

    again

--- src/server/routes/authRoutes.js
+++ src/server/routes/authRoutes.js
@@ -11,7 +11,7 @@ import { verifyEmail } from '../utils/verifyEmail.js';
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:4173'],
+  origin: ['http://localhost:4173', 'https://reo-test.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
